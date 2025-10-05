a96d1f120048fb5dd998ada465a368867ea833c4
ekelemene <ksuxaskrip2007@gmail.com>
2025-10-05 23:24:24 +0300

    123
    123

---
+++ CalculatorLib/CalculatorLib/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            // ❌ Ошибка специально, чтобы тест показал проблему:
+            return a * b; // a + b
+        }
+    }
+}
---
+++ CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,38 @@
+﻿
+using CalculatorLib;
+using Xunit;
+
+namespace CalculatorLibUnitTests
+{
+    public class CalculatorUnitTests
+    {
+        [Fact]
+        public void TestAdding2And2()
+        {
+            // размещение (Arrange)
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            Calculator calc = new();
+
+            // действие (Act)
+            double actual = calc.Add(a, b);
+
+            // утверждение (Assert)
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2And3()
+        {
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            Calculator calc = new();
+
+            double actual = calc.Add(a, b);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
